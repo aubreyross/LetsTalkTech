7c044d7e6f6e43ce525a1fea94860f5032bb35ab
Aubrey Ross <aubrey.ross04@gmail.com>
2022-01-16 20:41:00 -0600

    adjusted controllers and added starter files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const commentRoutes = require('./commentRoutes')
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const postRoutes = require('./postRoutes');
 
+router.use('/comments', commentRoutes);
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
