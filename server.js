5efbd8e062a1bcc437ca3d77f394625304e67576
Aubrey Ross <aubrey.ross04@gmail.com>
2022-01-16 20:56:56 -0600

    updated server.js file

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const session = require('express-session');
+const expresshb = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const path = require('path');
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
@@ -18,11 +21,15 @@ const sess = {
   })
 };
 
-app.use(session(sess));
+const hbs = expresshb.create ({ helpers });
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
