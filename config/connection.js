105bfbfd924b9aff836d75ec1fb9002e5bf69bce
aubreyross <aubrey.ross04@gmail.com>
2022-02-20 16:51:45 -0600

    adjusting connection.js issue

--- config/connection.js
+++ config/connection.js
@@ -13,8 +13,9 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: '127.0.0.1',
-      dialect: 'mysql'
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
     }
   );
 }
