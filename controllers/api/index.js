2d0be58ad2a733bfa0353d160d57792b4a731960
aubreyross <aubrey.ross04@gmail.com>
2022-02-20 15:44:38 -0600

    fixed plurals in  api route index.js file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,8 +4,8 @@ const commentRoutes = require('./commentRoutes')
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 
-router.use('/comments', commentRoutes);
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
+router.use('/comment', commentRoutes);
+router.use('/user', userRoutes);
+router.use('/post', postRoutes);
 
 module.exports = router;
