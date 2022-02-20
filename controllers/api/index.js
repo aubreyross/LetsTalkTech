c9ca4190e65b3e72c05286a0e475c9cb9825d683
aubreyross <aubrey.ross04@gmail.com>
2022-02-20 15:53:38 -0600

    refactored index.js file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 //required routes
 const router = require('express').Router();
-const commentRoutes = require('./commentRoutes')
+
+const commentRoutes = require('./commentRoutes');
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 
+//route paths for API
 router.use('/comment', commentRoutes);
 router.use('/user', userRoutes);
 router.use('/post', postRoutes);
